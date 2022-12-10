b56dd7f3c8b3bddc8edc2abee2835dc9a46b73ad
dgvalerio <al.davigvalerio@gmail.com>
2022-12-10 00:06:50 -0300

    feat: init project with eslint configurated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App: NextPage<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+      <title>Github Manager</title>
+      <meta name="description" content="The assistant for Github" />
+      <link rel="icon" href="/fav.png" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
+
+export default App;
